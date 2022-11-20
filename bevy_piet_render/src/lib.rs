d2a09661a43d7d594658c8e2db807125e6164096
Konstantin Yeftifeyev <zoxo@outlook.com>
2022-11-20 16:43:26 +0200

    feat: Add render lib

---
+++ bevy_piet_render/src/lib.rs
@@ -0,0 +1,7 @@
+/// Contains the Bevy interface to the Piet renderer.
+#[derive(Default)]
+pub struct PietRenderPlugin;
+
+impl Plugin for PietRenderPlugin {
+    fn build(&self, app: &mut App) {}
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,1 @@
-fn main() {
-    println!("Hello, world!");
-}
+pub use bevy_piet_core::*;
