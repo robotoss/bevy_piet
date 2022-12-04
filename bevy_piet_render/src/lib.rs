c6f56d765b3bbcf9f31eb4c307510568b37661c3
Konstantin Yeftifeyev <zoxo@outlook.com>
2022-12-04 17:05:25 +0200

    feat: Add run render function

--- bevy_piet_render/src/lib.rs
+++ bevy_piet_render/src/lib.rs
@@ -1,8 +1,14 @@
-use bevy::{prelude::{App, Plugin, Resource}, render::renderer::RenderDevice};
+use bevy::{
+    prelude::{App, Plugin, Resource},
+    render::renderer::RenderDevice,
+};
+use piet_scene::{Scene, SceneBuilder};
 use piet_wgsl::Renderer;
+use wgpu::{Device, Queue, TextureView};
 mod render;
+mod test_scene;
 
-#[derive(Resource,)]
+#[derive(Resource)]
 pub struct PietRenderResources {
     pub render: Renderer,
 }
@@ -14,7 +20,7 @@ pub struct PietRenderPlugin;
 impl Plugin for PietRenderPlugin {
     fn build(&self, app: &mut App) {
         let mut render_app = App::empty();
-       let render_device = app.world.resource::<RenderDevice>().clone();
+        let render_device = app.world.resource::<RenderDevice>().clone();
 
         let render = Renderer::new(render_device.wgpu_device()).expect("Can't create new Renderer");
 
@@ -22,8 +28,23 @@ impl Plugin for PietRenderPlugin {
     }
 }
 
-pub async fn run_render(
+pub fn run_render(
+    device: &Device,
+    mut render: Renderer,
+    queue: &Queue,
+    texture: &TextureView,
+    width: u32,
+    height: u32,
 ) -> Result<(), Box<dyn std::error::Error>> {
+    let mut scene = Scene::default();
+
+    let mut builder = SceneBuilder::for_scene(&mut scene);
+    test_scene::render_funky_paths(&mut builder);
+    builder.finish();
+
+    render
+        .render_to_texture(device, queue, &scene, texture, width, height)
+        .expect("failed to render to texture");
 
     Ok(())
 }
